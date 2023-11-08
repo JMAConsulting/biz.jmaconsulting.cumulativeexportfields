180ce57545ee83c1f295aeeffcd200a53f93db04
pipeline <pipeline@jmaconsulting.biz>
2023-11-08 14:18:15 -0700

    Civix Upgrade to 23.11.8

--- cumulativeexportfields.php
+++ cumulativeexportfields.php
@@ -12,17 +12,6 @@ function cumulativeexportfields_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function cumulativeexportfields_civicrm_xmlMenu(&$files) {
-  _cumulativeexportfields_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -74,7 +63,6 @@ function cumulativeexportfields_civicrm_uninstall() {
       ),
     ));
   }
-  _cumulativeexportfields_civix_civicrm_uninstall();
 }
 
 /**
@@ -106,56 +94,6 @@ function cumulativeexportfields_civicrm_disable() {
       'is_active' => 0,
     ),
   ));
-  _cumulativeexportfields_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function cumulativeexportfields_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _cumulativeexportfields_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function cumulativeexportfields_civicrm_managed(&$entities) {
-  _cumulativeexportfields_civix_civicrm_managed($entities);
-}
-
-/**
- * Implementation of hook_civicrm_caseTypes
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function cumulativeexportfields_civicrm_caseTypes(&$caseTypes) {
-  _cumulativeexportfields_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implementation of hook_civicrm_alterSettingsFolders
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function cumulativeexportfields_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _cumulativeexportfields_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -239,7 +177,6 @@ function cumulativeexportfields_civicrm_export(&$exportTempTable, &$headerRows, 
     }
   }
 }
-
 
 /**
  * @param array $contactIDs
